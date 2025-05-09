909c8cc0efd5adb397d0faa2008ea1d66fbd0256
tpwong <115126035+tpwong@users.noreply.github.com>
2025-05-09 15:37:42 +0800

    Update tmp.cs

--- tmp.cs
+++ tmp.cs
@@ -1,3 +1,3 @@
-CREATE INDEX idx_rating_dead_letters_gaming_dt 
-ON earning.rating_dead_letters ((to_date(message_value->>'GamingDt', 'YYYY-MM-DD')))
-WHERE message_value->>'GamingDt' IS NOT NULL;
+CREATE INDEX idx_rating_dead_letters_gaming_dt
+ON earning.rating_dead_letters ((message_value->'GamingDt')::jsonb::text::date)
+WHERE message_value ? 'GamingDt';
