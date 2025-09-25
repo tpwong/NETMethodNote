b227239c60bbd6b6280ef1ce498c40408d10a640
tpwong <115126035+tpwong@users.noreply.github.com>
2025-09-25 23:57:01 +0800

    Update tmp.cs

--- tmp.cs
+++ tmp.cs
@@ -1,10 +1,4 @@
--- 1. 刪除舊的 BRIN 索引
-DROP INDEX earning.bucket_earned_transactions_hub_tran_idx;
-
--- 2. 建立新的 B-Tree 索引 (這是正確的選擇)
---    同樣使用部分索引 (Partial Index) 來縮小索引體積，只索引未作廢的記錄
-CREATE INDEX bucket_earned_transactions_hub_tran_idx 
-ON earning.bucket_earned_transactions (hub_tran_id) 
-WHERE (NOT is_void);
-
-
+-- 為 4 月份分區添加約束的範例
+ALTER TABLE earning.bucket_earned_import_migration_new_1 
+ADD CONSTRAINT bucket_earned_import_migration_new_1_gaming_dt_check 
+CHECK (gaming_dt >= '2025-04-01' AND gaming_dt < '2025-05-01');
