16f3b7bcf85a3798c007ef240e27bb9577cd276c
tpwong <115126035+tpwong@users.noreply.github.com>
2025-12-02 21:27:37 +0800

    Update tmp.cs

--- tmp.cs
+++ tmp.cs
@@ -1,14 +1,3 @@
-UPDATE earning_qualified_ratings t
-SET acct      = @newAcct,
-    player_id = @newPlayerId,
-    updated_at = CURRENT_TIMESTAMP
-WHERE t.acct = @oldAcct
-  AND NOT EXISTS (
-      SELECT 1
-      FROM earning_qualified_ratings x
-      WHERE x.acct        = @newAcct
-        AND x.gaming_dt   = t.gaming_dt
-        AND x.casino_code = t.casino_code
-        AND x.dept_code   = t.dept_code
-        AND x.tran_id     = t.tran_id
-  );
+var jitter = Random.Shared.Next(0, baseDelayMs);
+var delay = (int)(baseDelayMs * Math.Pow(2, attempt - 1)) + jitter;
+await Task.Delay(delay);
